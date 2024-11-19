673b789747f68f2764b1ae3a21e33faae3d1377d
KDThaGoatt <kyand906@deltalearns.ca>
2024-11-19 10:55:12 -0800

    a

--- problem1.py
+++ problem1.py
@@ -28,7 +28,30 @@ The roots are -4.0 and -4.0
 """
 import os
 os.system('cls')
-
+import math
 
 print("Enter in the coefficients for a quadratic equation in the format:")
 print("  ax^2 + bx + c = 0")
+
+a = (input("a = "))
+b = (input("b = "))
+c = (input("c = "))
+
+try:
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    firstEq = (-b + math.sqrt(b ** 2 - 4*a*c)) / (2*a)
+    secondEq = (-b - math.sqrt(b ** 2 - 4*a*c)) / (2*a)
+    roundOne = round(firstEq, 2)
+    roundTwo = round(secondEq, 2)
+    print(f"The roots are {roundOne} and {roundTwo}")
+except:
+    try:
+        a = float(a)
+        b = float(b)
+        c = float(c)
+        print("There are no real roots to the equation")
+
+    except:
+        print("Those are not valid values for a, b or c")
